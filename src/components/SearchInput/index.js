babeed63ae97810d284c382eab84d356801a8b1c
ldickmann <ldickmann12@gmail.com>
2025-02-25 16:54:02 -0300

    icon add no componente SearchInput

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -4,13 +4,18 @@ import styles from "./SearchInput.module.css";
 // Componente de input de pesquisa
 function SearchInput({ value, onChange }) {
   return (
-    <div className={styles.searchContainer}>
+    <div className={styles.containerInput}>
       <input
         className={styles.input}
         type="text"
         value={value}
         onChange={onChange}
         placeholder="Pesquisar"
+      />
+      <img
+        src="/images/icons/search.svg"
+        alt="Ícone de pesquisa"
+        className={styles.searchIcon}
       />
     </div>
   );
